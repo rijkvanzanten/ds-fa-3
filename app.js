c8b90ffc2b432780e802190e766a038efcf77350
rijkvanzanten <rijkvanzanten@me.com>
2018-12-14 16:55:50 -0500

    Add TCP server

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const WebSocket = require("ws");
+const net = require("net");
+const EventEmitter = require("events");
 
-const app = express()
-  .use(express.static("./public"));
+const emitter = new EventEmitter();
 
-module.exports = app;
+const port = process.env.PORT || 3000;
+const TCPPort = port + 1;
+
+const app = express().use(express.static("./public"));
+
+const server = require("http").createServer(app);
+const TCPServer = net.createServer();
+
+const wss = new WebSocket.Server({ server });
+
+
+wss.on("connection", ws => {
+  emitter.on("data", data => {
+    ws.send(data)
+  });
+});
+
+server.listen(port, () => console.log("Server started."));
+TCPServer.listen(TCPPort, () => console.log("TCP Server Started"));
+
+TCPServer.on("connection", sock => {
+  sock.on("data", data => {
+    console.log(data);
+    emitter.emit("data", data);
+  });
+});
--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const server = require("http").createServer(app);
-
-require("./socket")(server);
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => console.log("Server started."));
