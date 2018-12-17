e578d3c590afdf53d0585ea526320ad80eee2115
rijkvanzanten <rijkvanzanten@me.com>
2018-12-16 19:46:44 -0500

    Use incoming Y value for synth frequency

--- public/script.js
+++ public/script.js
@@ -1,6 +1,14 @@
 const socket = new WebSocket("ws://localhost:3000");
 const outputEl = document.querySelector("#output");
 
+const synth = new (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext)();
+const osc = synth.createOscillator();
+const gain = synth.createGain();
+
+osc.connect(gain);
+osc.frequency.value = 500;
+gain.gain.value = 100;
+
 const calibrationSize = 20;
 let calibrationCounter = 0;
 let median = 0;
@@ -8,12 +16,12 @@ let state = "CALIBRATING";
 
 // This is a 50ms loop
 socket.onmessage = function(event) {
-  const rawZ = Number(event.data);
-  if (rawZ < 1500 || rawZ > 2500) return;
+  const rawY = Number(event.data);
+  if (rawY < 1500 || rawY > 2500) return;
 
   if (state === "CALIBRATING") {
     if (calibrationCounter < calibrationSize) {
-      median += rawZ;
+      median += rawY;
       calibrationCounter++;
     } else if (calibrationCounter === calibrationSize) {
       median = Math.floor(median / calibrationSize);
@@ -22,8 +30,9 @@ socket.onmessage = function(event) {
   }
 
   if (state === "RUNNING") {
-    const diff = rawZ - median;
+    const diff = rawY - median;
     outputEl.innerText = diff;
+    osc.frequency.value = 500 - diff;
   }
 };
 
@@ -31,4 +40,17 @@ document.querySelector("#calibrate").addEventListener("click", () => {
   median = 0;
   calibrationCounter = 0;
   state = "CALIBRATING";
+});
+
+document.querySelector("#start").addEventListener("click", () => {
+  gain.connect(synth.destination);
+  try {
+    osc.start();
+  } catch(e) {
+    // we don't care that it already started
+  }
+});
+
+document.querySelector("#stop").addEventListener("click", () => {
+  gain.disconnect(synth.destination);
 });
